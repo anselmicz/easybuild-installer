c60516e3f2e4560fc61d672e34aa7b206a368892
anselmicz <jakubkropacek@email.cz>
2023-01-10 19:33:22 +0100

    Add '--user' flag to pip installation of Python packages to make it explicit.

--- scripts/01-os_dependencies.sh
+++ scripts/01-os_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo apt install -y wget gcc g++ make rsync tclsh tcl-dev libreadline-dev libibverbs-dev python3-pip xdot patch
-python3 -m pip install --upgrade python-graph-core python-graph-dot archspec autopep8 GitPython pep8 pycodestyle Rich setuptools
+python3 -m pip install --user --upgrade python-graph-core python-graph-dot archspec autopep8 GitPython pep8 pycodestyle Rich setuptools
